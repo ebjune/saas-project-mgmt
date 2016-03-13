5261c56b6fea6aafbfe95d44b51c0e0eb2b78e42
Erika Bjune <erika.bjune@gmail.com>
2016-03-13 18:53:16 +0000

    Added Stripe, accept payments, update registrations and update tenant

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,19 @@ Rails.application.routes.draw do
 
    root :to => "home#index"
 
-    
+
   # *MUST* come *BEFORE* devise's definitions (below)
-  as :user do   
+  as :user do
     match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
 
-  devise_for :users, :controllers => { 
-    :registrations => "milia/registrations",
+  devise_for :users, :controllers => {
+    :registrations => "registrations",
     :confirmations => "confirmations",
-    :sessions => "milia/sessions", 
-    :passwords => "milia/passwords", 
+    :sessions => "milia/sessions",
+    :passwords => "milia/passwords",
   }
 
+  match '/plan/edit' => 'tenants#edit', via: :get, as: :edit_plan
+  match '/plan/update' => 'tenants#update', via: [:put, :patch], as: :update_plan
 end
